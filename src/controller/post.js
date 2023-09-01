ddf035cade0f1243b7c59a41ff66a8e59667f10f
hotteok00 <hojun7889@gmail.com>
2023-09-01 22:52:51 +0900

    Feat: post routing

--- src/controller/post.js
+++ src/controller/post.js
@@ -5,63 +5,63 @@ const router = express.Router();
 const PATH = process.env.SERVER_PATH;
 
 // model
-const {totalPostFromUserIdArray, getPostFromUserIdArray} = require(PATH + '/src/model/post');
-const {getFriendFromUserId} = require(PATH + '/src/model/friend');
-const {getImgByPostId} = require(PATH + '/src/model/image');
-const {getCommentByPostId} = require(PATH + '/src/model/comment');
-const {getUserInfoByUserId} = require(PATH + '/src/model/account');
+const { totalPostFromUserIdArray, getPostFromUserIdArray } = require(PATH + '/src/model/post');
+const { getFriendFromUserId } = require(PATH + '/src/model/friend');
+const { getImgByPostId } = require(PATH + '/src/model/image');
+const { getCommentByPostId } = require(PATH + '/src/model/comment');
+const { getUserInfoByUserId } = require(PATH + '/src/model/account');
 
-router.post('/', async (req, res)=>{
-    try{
+router.post('/', async (req, res) => {
+    try {
         const contentId = req.body.contentId;
         const requestValue = req.body.requestValue;
         const userId = res.locals.userId;
-        const [data,state] = await getFriendFromUserId(userId);
-        if(state !== "SUCCESS"){
+        const [data, state] = await getFriendFromUserId(userId);
+        if (state !== "SUCCESS") {
             throw data;
         }
         const friends = data.map((friend) => friend.dataValues.friendId);
         const [totalPage, totalPageState] = await totalPostFromUserIdArray(friends);
-        if(totalPageState !== "SUCCESS"){
+        if (totalPageState !== "SUCCESS") {
             throw totalPage;
         }
         const [posts, postsState] = await getPostFromUserIdArray(friends, contentId, requestValue);
-        if(postsState !== "SUCCESS"){
+        if (postsState !== "SUCCESS") {
             throw posts;
         }
-        const postsFunction = posts.map(async post =>{
+        const postsFunction = posts.map(async post => {
             const [imgs, state] = await getImgByPostId(post.id);
-            if(state !== "SUCCESS"){
+            if (state !== "SUCCESS") {
                 throw imgs;
             }
             const [comments, commentsState] = await getCommentByPostId(post.id, null, 10);
-            if(commentsState !== "SUCCESS"){
+            if (commentsState !== "SUCCESS") {
                 throw commentsState;
             }
-            const commentsFunction = comments.map( async comment => {
+            const commentsFunction = comments.map(async comment => {
                 const [userInfo, userInfoState] = await getUserInfoByUserId(comment.userId);
-                if(userInfoState !== "SUCCESS"){
+                if (userInfoState !== "SUCCESS") {
                     throw userInfo;
                 }
                 return {
                     commentId: comment.id,
-                    author:{
+                    author: {
                         id: comment.userId,
                         img: userInfo.imageUrl,
                         name: userInfo.name
                     },
-                    createdAt:comment.createdAt.getTime(),
+                    createdAt: comment.createdAt.getTime(),
                     content: comment.content
                 }
             });
             const commentsArray = await Promise.all(commentsFunction);
-            const imgsArray = imgs.map(img=>img.dataValues.url);
+            const imgsArray = imgs.map(img => img.dataValues.url);
             const [userInfo, userInfoState] = await getUserInfoByUserId(post.userId);
-            if(userInfoState !== "SUCCESS"){
+            if (userInfoState !== "SUCCESS") {
                 throw userInfo;
             }
             return {
-                id:post.id,
+                id: post.id,
                 author: {
                     id: post.userId,
                     img: userInfo.imageUrl,
@@ -71,24 +71,36 @@ router.post('/', async (req, res)=>{
                 imgUrl: imgsArray,
                 content: post.content,
                 comments: commentsArray,
-                tags: ["사람","css","뒤져","망할","리액트","버튜얼라이즈","사라져"] //임시 설정
+                tags: ["사람", "css", "뒤져", "망할", "리액트", "버튜얼라이즈", "사라져"] //임시 설정
             }
         });
 
         const postsArray = await Promise.all(postsFunction);
         res.json({
             state: "SUCCESS",
-            payload:{
+            payload: {
                 totalPage: totalPage,
                 data: postsArray
             }
         });
 
-    }catch(err){
+    } catch (err) {
         console.error(err);
         res.status(500);
-        res.json({state:"ERROR"});
+        res.json({ state: "ERROR" });
     }
 });
+
+router.post('/add', async (req, res) => {
+
+})
+
+router.post('/remove', async (req, res) => {
+
+})
+
+router.post('/modify', async (req, res) => {
+
+})
 
 module.exports = router;
--- src/controller/signUp.js
+++ src/controller/signUp.js
@@ -7,30 +7,30 @@ const PATH = process.env.SERVER_PATH;
 const DOMAIN = process.env.SERVER_DOMAIN;
 
 // model
-const {register} = require(PATH + '/src/model/account');
+const { register } = require(PATH + '/src/model/account');
 
-router.post('/', async (req, res)=>{
-    try{
+router.post('/', async (req, res) => {
+    try {
         const email = req.body.email;
         const password = req.body.password;
         const name = req.body.name;
         const phoneNumber = req.body.phoneNumber;
-        const imageUrl = DOMAIN+"/image/defaultImage.jpg";
+        const imageUrl = DOMAIN + "/image/defaultImage.jpg";
         const [data, status] = await register(email, password, name, phoneNumber, imageUrl);
-        switch(status) {
+        switch (status) {
             case "SUCCESS":
-                res.json({state:"SUCCESS"});
+                res.json({ state: "SUCCESS" });
                 break;
             case "FAILURE":
-                res.json({state:"FAILURE", data});
+                res.json({ state: "FAILURE", data });
                 break;
             default:
                 throw data;
         }
-    }catch(err){
+    } catch (err) {
         console.error(err);
         res.status(500);
-        res.json({state:"ERROR"});
+        res.json({ state: "ERROR" });
     }
 });
 
